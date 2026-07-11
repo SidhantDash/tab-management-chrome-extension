f842abbc5d934b47e2ea2bda4c52d19773778814
Sidhant <sidhantdash126@gmail.com>
2026-07-11 18:57:12 -0400

    Added sort YT tabs button

--- tab-management.js
+++ tab-management.js
@@ -1,9 +1,21 @@
-console.log("Hey");
-
 const tabs = await chrome.tabs.query({});
 
-console.log(tabs.length);
+// console.log(tabs.length);
 
 for (const tab of tabs) {
-    console.log(tab.title);
+    console.log("ID: " + tab.id + ", Title: " + tab.title + ", URL: " + tab.url);
 }
+
+const ytSortButton = document.getElementById("yt-sort-button");
+ytSortButton.addEventListener("click", async () => {
+
+    for (let i = tabs.length - 1; i >= 0; i--) {
+        let ytCounter = 0;
+        if (tabs[i].url.includes('www.youtube.com/')) {
+            ytCounter += 1;
+            console.log(tabs[i].title);
+            await chrome.tabs.move(tabs[i].id, { index: 0 });
+        }
+        console.log("Total YT tabs: " + ytCounter);
+    }
+});
