040d38a55d8f7bbe4a9b6080f073c0e1016054ae
Sidhant <sidhantdash126@gmail.com>
2026-07-11 19:15:10 -0400

    Removes tabs with bare yt links

--- tab-management.js
+++ tab-management.js
@@ -11,7 +11,11 @@ ytSortButton.addEventListener("click", async () => {
 
     for (let i = tabs.length - 1; i >= 0; i--) {
         let ytCounter = 0;
-        if (tabs[i].url.includes('www.youtube.com/')) {
+        if (tabs[i].url === "https://www.youtube.com/") {
+            console.log("Bare YT Link Detected")
+            await chrome.tabs.remove(tabs[i].id);
+        }
+        else if (tabs[i].url.includes('www.youtube.com/')) {
             ytCounter += 1;
             console.log(tabs[i].title);
             await chrome.tabs.move(tabs[i].id, { index: 0 });
